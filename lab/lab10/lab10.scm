36eaa685c44be47093b678fd771861986755d6f0
f1a3h <ihlwer@outlook.com>
2023-10-03 15:09:15 +0800

    finish lab10

---
+++ lab/lab10/lab10.scm
@@ -0,0 +1,126 @@
+(define (over-or-under num1 num2)
+  'YOUR-CODE-HERE
+  (cond
+    ((> num1 num2) '1)
+    ((= num1 num2) '0)
+    (else '-1)
+  )
+)
+
+;;; Tests
+(over-or-under 1 2)
+; expect -1
+(over-or-under 2 1)
+; expect 1
+(over-or-under 1 1)
+; expect 0
+
+
+(define (filter-lst fn lst)
+  'YOUR-CODE-HERE
+  (if (null? lst)
+    nil
+    (if (fn (car lst))
+        (cons (car lst) (filter-lst fn (cdr lst)))
+        (filter-lst fn (cdr lst))
+    )
+  )
+)
+
+;;; Tests
+(define (even? x)
+  (= (modulo x 2) 0))
+(filter-lst even? '(0 1 1 2 3 5 8))
+; expect (0 2 8)
+
+
+(define (make-adder num)
+  'YOUR-CODE-HERE
+  (lambda (inc) (+ num inc))
+)
+
+;;; Tests
+(define adder (make-adder 5))
+(adder 8)
+; expect 13
+
+
+(define lst
+  'YOUR-CODE-HERE
+  (cons (cons 1 nil)
+    (cons 2
+      (cons (cons 3 4)
+        5
+      )
+    )
+  )
+)
+
+
+(define (composed f g)
+  'YOUR-CODE-HERE
+  (lambda (x) (f (g x)))
+)
+
+
+(define (remove item lst)
+  'YOUR-CODE-HERE
+  (filter-lst (lambda (x) (= x item))
+    lst
+  )
+)
+
+
+;;; Tests
+(remove 3 nil)
+; expect ()
+(remove 3 '(1 3 5))
+; expect (1 5)
+(remove 5 '(5 3 5 5 1 4 5 4))
+; expect (3 1 4 4)
+
+
+(define (no-repeats s)
+  'YOUR-CODE-HERE
+  (if (null? s)
+    nil
+    (cons (car s)
+      (no-repeats
+        (filter-lst (lambda (x) (not (= x (car s))))
+          (cdr s)
+        )
+      )
+    )
+  )
+)
+
+
+(define (substitute s old new)
+  'YOUR-CODE-HERE
+  (if (null? s)
+    nil
+    (if (pair? (car s))
+      (cons (substitute (car s) old new)
+        (substitute (cdr s) old new)
+      )
+      (cons (if (eq? (car s) old)
+          new
+          (car s))
+        (substitute (cdr s) old new)
+      )
+    )
+  )
+)
+
+
+(define (sub-all s olds news)
+  'YOUR-CODE-HERE
+  (if (null? olds)
+    s
+    (sub-all (substitute s (car olds) (car news))
+      (cdr olds)
+      (cdr news)
+    )
+  )
+)
+
