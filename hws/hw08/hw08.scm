b195a5ffed54463e1f9d3ccc82e017eb652db9f8
f1a3h <ihlwer@outlook.com>
2023-10-26 19:21:01 +0800

    finish hw08

--- hws/hw08/hw08.scm
+++ hws/hw08/hw08.scm
@@ -1,11 +1,46 @@
 (define (rle s)
   'YOUR-CODE-HERE
+  (define (helper x cnt s)
+    (if (null? s)
+        (list x cnt)
+        (if (not (= x (car s)))
+            (list x cnt)
+            (helper x (+ cnt 1) (cdr-stream s)))))
+
+  (define (next x s)
+    (if (null? s)
+        nil
+        (if (= x (car s))
+            (next x (cdr-stream s))
+            s)))
+
+  (if (null? s)
+    nil
+    (cons-stream (helper (car s) 0 s) (rle (next (car s) s))))
 )
 
 
 
 (define (group-by-nondecreasing s)
-    'YOUR-CODE-HERE)
+    'YOUR-CODE-HERE
+    (define (helper last s)
+        (if (null? s)
+            (list last)
+            (if (<= last (car s))
+                (cons last (helper (car s) (cdr-stream s)))
+                (list last))))
+
+    (define (next last s)
+        (if (null? s)
+            nil
+            (if (<= last (car s))
+                (next (car s) (cdr-stream s))
+                s)))
+
+    (if (null? s)
+        nil
+        (cons-stream (helper (car s) (cdr-stream s)) (group-by-nondecreasing (next (car s) (cdr-stream s)))))
+)
 
 
 (define finite-test-stream
--- hws/hw08/hw08.sql
+++ hws/hw08/hw08.sql
@@ -26,29 +26,45 @@ CREATE TABLE sizes AS
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.name, b.size FROM dogs AS a, sizes AS b WHERE a.height > b.min AND a.height <= b.max;
 
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_parent_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.child FROM parents AS a, dogs AS b WHERE a.parent = b.name ORDER BY b.height DESC;
 
 
 -- Filling out this helper table is optional
 CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.child AS sib1, b.child AS sib2
+  FROM parents AS a, parents AS b
+  WHERE a.parent = b.parent AND a.child < b.child;
 
 -- Sentences about siblings that are the same size
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT sib1 || " and " || sib2 || " are " || b.size || " siblings"
+  FROM siblings, size_of_dogs AS a, size_of_dogs AS b
+  WHERE a.name = sib1 AND b.name = sib2 AND a.size = b.size;
 
 
 -- Ways to stack 4 dogs to a height of at least 170, ordered by total height
 CREATE TABLE stacks_helper(dogs, stack_height, last_height, n);
 
 -- Add your INSERT INTOs here
-
+INSERT INTO stacks_helper(dogs, stack_height, last_height, n) SELECT name, height, height, 1 FROM dogs;
+INSERT INTO stacks_helper(dogs, stack_height, last_height, n)
+  SELECT a.dogs || ", " || b.name, a.stack_height + b.height, b.height, 2
+  FROM stacks_helper AS a, dogs AS b
+  WHERE a.n = 1 AND b.height > a.last_height;
+INSERT INTO stacks_helper(dogs, stack_height, last_height, n)
+  SELECT a.dogs || ", " || b.name, a.stack_height + b.height, b.height, 3
+  FROM stacks_helper AS a, dogs AS b
+  WHERE a.n = 2 AND b.height > a.last_height;
+INSERT INTO stacks_helper(dogs, stack_height, last_height, n)
+  SELECT a.dogs || ", " || b.name, a.stack_height + b.height, b.height, 4
+  FROM stacks_helper AS a, dogs AS b
+  WHERE a.n = 3 AND b.height > a.last_height;
 
 CREATE TABLE stacks AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT dogs, stack_height FROM stacks_helper WHERE n = 4 AND stack_height >= 170 ORDER BY stack_height;
 
