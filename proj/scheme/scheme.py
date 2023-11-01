aa66136458ea18fa2762278d770acd11ede5c4d6
f1a3h <ihlwer@outlook.com>
2023-11-01 17:53:41 +0800

    pass lab14 q02

--- proj/scheme/scheme.py
+++ proj/scheme/scheme.py
@@ -93,12 +93,15 @@ class Frame(object):
         """Define Scheme SYMBOL to have VALUE."""
         # BEGIN PROBLEM 2
         "*** YOUR CODE HERE ***"
+        self.bindings[symbol] = value
         # END PROBLEM 2
 
     def lookup(self, symbol):
         """Return the value bound to SYMBOL. Errors if SYMBOL is not found."""
         # BEGIN PROBLEM 2
         "*** YOUR CODE HERE ***"
+        if symbol in self.bindings: return self.bindings[symbol]
+        elif not self.parent is None: return self.parent.lookup(symbol)
         # END PROBLEM 2
         raise SchemeError('unknown identifier: {0}'.format(symbol))
 
